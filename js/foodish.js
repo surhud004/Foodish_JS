eac2340c7b24fb50f3a08fd405736c99555f421f
surhud004 <surhud004@gmail.com>
2022-02-14 17:46:00 -0500

    Update foodish scripts

--- js/foodish.js
+++ js/foodish.js
@@ -1,29 +1,29 @@
 const getFoodish = (categoryName) => {
   let catchOfTheDay;
-  switch (categoryName) {
-    case "random":
-      {
-        const menuDB = Object.keys(images);
-        const randomCategorySelector = Math.floor(
-          Math.random() * menuDB.length
-        );
-        const anyRandomFood = menuDB[randomCategorySelector];
-        const foodDB = images[anyRandomFood];
-        const randomSelector = Math.floor(
-          Math.random() * (Object.keys(foodDB).length - 1) + 1
-        );
-        catchOfTheDay = foodDB[randomSelector];
-      }
-      break;
-    case "biryani":
-      {
-        const foodDB = images.biryani;
-        const randomSelector = Math.floor(
-          Math.random() * (Object.keys(foodDB).length - 1) + 1
-        );
-        catchOfTheDay = foodDB[randomSelector];
-      }
-      break;
+  const menuDB = Object.keys(images);
+
+  if (categoryName === "random") {
+    const randomCategorySelector = Math.floor(Math.random() * menuDB.length);
+    const anyRandomFood = menuDB[randomCategorySelector];
+    const foodDB = images[anyRandomFood];
+    const randomSelector = Math.floor(
+      Math.random() * (Object.keys(foodDB).length - 1) + 1
+    );
+    catchOfTheDay = foodDB[randomSelector];
+  } else if (categoryName === "butter-chicken") {
+    const foodDB = images.butterChicken;
+    const randomSelector = Math.floor(
+      Math.random() * (Object.keys(foodDB).length - 1) + 1
+    );
+    catchOfTheDay = foodDB[randomSelector];
+  } else if (menuDB.includes(categoryName)) {
+    const foodDB = images[categoryName];
+    const randomSelector = Math.floor(
+      Math.random() * (Object.keys(foodDB).length - 1) + 1
+    );
+    catchOfTheDay = foodDB[randomSelector];
+  } else {
+    catchOfTheDay = "";
   }
 
   document.getElementById("foodish").src = `../${catchOfTheDay}`;
--- js/script.js
+++ js/script.js
@@ -1,41 +1,41 @@
 const getFoodish = (pageName) => {
   let catchOfTheDay;
-  switch (pageName) {
-    case "index":
-      {
-        // menuDB is [biryani, burger, pizza]
-        const menuDB = Object.keys(images);
-        // random number generator within menuDB array range
-        const randomCategorySelector = Math.floor(
-          Math.random() * menuDB.length
-        );
-        // anyRandomFood is burger
-        const anyRandomFood = menuDB[randomCategorySelector];
-        // foodDB is {1: ../burger1.jpg, 2: ../burger2.jpg}
-        const foodDB = images[anyRandomFood];
-        // random number generator within foodDB object key range
-        const randomSelector = Math.floor(
-          Math.random() * (Object.keys(foodDB).length - 1) + 1
-        );
-        // catchOfTheDay is ../burger101.jpg
-        catchOfTheDay = foodDB[randomSelector];
-      }
-      break;
-    case "biryani":
-      {
-        const foodDB = images.biryani;
-        const randomSelector = Math.floor(
-          Math.random() * (Object.keys(foodDB).length - 1) + 1
-        );
-        catchOfTheDay = foodDB[randomSelector];
-      }
-      break;
+  // menuDB is [biryani, burger, pizza]
+  const menuDB = Object.keys(images);
+
+  if (pageName === "index") {
+    // random number generator within menuDB array range
+    const randomCategorySelector = Math.floor(Math.random() * menuDB.length);
+    // anyRandomFood is burger
+    const anyRandomFood = menuDB[randomCategorySelector];
+    // foodDB is {1: ../burger1.jpg, 2: ../burger2.jpg}
+    const foodDB = images[anyRandomFood];
+    // random number generator within foodDB object key range
+    const randomSelector = Math.floor(
+      Math.random() * (Object.keys(foodDB).length - 1) + 1
+    );
+    // catchOfTheDay is ../burger101.jpg
+    catchOfTheDay = foodDB[randomSelector];
+  } else if (pageName === "butter-chicken") {
+    const foodDB = images.butterChicken;
+    const randomSelector = Math.floor(
+      Math.random() * (Object.keys(foodDB).length - 1) + 1
+    );
+    catchOfTheDay = foodDB[randomSelector];
+  } else if (menuDB.includes(pageName)) {
+    const foodDB = images[pageName];
+    const randomSelector = Math.floor(
+      Math.random() * (Object.keys(foodDB).length - 1) + 1
+    );
+    catchOfTheDay = foodDB[randomSelector];
+  } else {
+    catchOfTheDay = "";
   }
 
   document.getElementById("foodish").src = catchOfTheDay;
   document.getElementById(
     "shareLink"
-  ).value = `https://foodish-api.netlify.app/${catchOfTheDay}`;
+  ).value = `https://foodish-js.netlify.app/${catchOfTheDay}`;
 };
 
 const imageCount = () => {
